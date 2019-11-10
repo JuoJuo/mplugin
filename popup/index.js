ba72409943815b6eb0d808017764e0129555cdaf
18883283205@163.com <18883283205@163.com>
2019-11-10 18:54:15 +0800

    remove ununsed code

--- popup/index.js
+++ popup/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
-import ViewUI from 'view-design';
+import ViewUI, { Input, Row, Col } from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import popup from './popup'
-import { Input, Row, Col, Tooltip } from 'view-design';
-
 
 Vue.component('Input', Input);
 Vue.component('Row', Row);
 Vue.component('Col', Col);
-Vue.component('Col', Tooltip);
 
 Vue.use(ViewUI,{
   size: 'small'
